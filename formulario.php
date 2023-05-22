47b49be5a6e82929c22f376741d383ab62038c02
S.S.I <70864023+manoper93@users.noreply.github.com>
2023-05-22 17:14:39 +0100

    Atualizar o formulario.php

--- formulario.php
+++ formulario.php
@@ -1,9 +1,26 @@
 <?php
+$host = 'localhost';
+$db = 'watt';
+$usuario = 'root';
+$senha = '';
 
-if (isset($_POST['nome'])) {
-    include 'conectar_db.php';
-    echo "Enviado!";
-} else {
-    echo "ERRO!"; 
+$conn = new mysqli($host, $usuario, $senha, $db);
+
+if ($conn->connect_error) {
+    die('Erro na conexão com o banco de dados: ' . $conn->connect_error);
 }
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$mensagem = $_POST['mensagem'];
+
+$sql = "INSERT INTO contacto (nome, email, mensagem) VALUES ('$nome', '$email', '$mensagem')";
+
+if ($conn->query($sql) === TRUE) {
+    echo 'Dados salvos com sucesso na base de dados.';
+} else {
+    echo 'Erro ao salvar os dados: ' . $conn->error;
+}
+
+$conn->close();
 ?>
